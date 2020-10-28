3ab03007b4a6820e4f7748e7d2b30a838eb1cba0
Satnam <satnam10virdi@gmail.com>
2020-10-28 19:47:22 +0530

    implemented notifications

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
-} from "react-router-dom";
+  Redirect,
+} from 'react-router-dom'
 
-import { Navbar } from "./app/Navbar";
+import { Navbar } from './app/Navbar'
 
-import { PostsList } from "./features/posts/PostsList";
-import { AddPostForm } from "./features/posts/AddPostForm";
-import { SinglePostPage } from "./features/posts/SinglePostPage";
-import {EditPostForm}  from './features/posts/EditPostForm'
+import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+import { EditPostForm } from './features/posts/EditPostForm'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
+import { NotificationsList } from './features/notifications/NotificationsList'
 
 function App() {
   return (
@@ -31,11 +34,14 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
+          <Route exact path="/notifications" component={NotificationsList} />
           <Redirect to="/" />
         </Switch>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,7 +1,7 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
 
-const initialState = [];
+const initialState = []
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
   const response = await client.get('/fakeApi/users')
@@ -9,14 +9,19 @@ export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
 })
 
 const usersSlice = createSlice({
-  name: "users",
+  name: 'users',
   initialState,
   reducers: {},
   extraReducers: {
     [fetchUsers.fulfilled]: (state, action) => {
       return action.payload
-    }
-  }
-});
+    },
+  },
+})
 
-export default usersSlice.reducer;
+export default usersSlice.reducer
+
+export const selectAllUsers = (state) => state.users
+
+export const selectUserById = (state, userId) =>
+  state.users.find((user) => user.id === userId)
